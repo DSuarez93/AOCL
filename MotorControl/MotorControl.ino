060d305f217dbec53844c13d7c7ea407a3ada53e
Damian <dsuarez1@stevens.edu>
2017-09-26 10:20:39 -0400

    Update .ino description
    This is an early attempt at a looping function for digital motors. It does not yet incorporate the Sabertooth motor drivers, but the logic can be applied similiarly.

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -2,24 +2,13 @@
   Motor Relay Power Control
   10-30mA for LEDs
   
+  This is a demonstration of the Arduino functionality with the digital motors.
  */
   
 //Motors
   const int Mo5 = 14;
   const int Mo1 = 17;
   const int Mo3 = 18;
-//Accelerometer
-//  const int Ascl = 82;  //Analog
-//  const int Asda = 83;
-//  const int Aint = 19;  //Digital
-//Relay Power Control
-//  const int ReM1 = 23;
-//  const int ReM2 = 24;
-//  const int ReM3 = 25;
-//  const int ReM4 = 26;
-//Button
-  const int butTon = 2;
-  int bState = 0;
 
 void setup() {
 Serial.begin(9600);
@@ -30,16 +19,8 @@ Serial.begin(9600);
   pinMode(Mo1, OUTPUT);
   pinMode(Mo3, OUTPUT);
   pinMode(Mo5, OUTPUT);
-/*
-  pinMode (Ascl, INPUT);
-  pinMode (Asda, INPUT);
-  pinMode (Aint, INPUT);
 
-  pinMode (ReM1, OUTPUT);
-  pinMode (ReM2, OUTPUT);
-  pinMode (ReM3, OUTPUT);
-  pinMode (ReM4, OUTPUT);
-*/
+//LEd blink twice, second blink is longer.
   digitalWrite(13, LOW);
   delay(200);
   digitalWrite(13, HIGH);
@@ -47,36 +28,21 @@ Serial.begin(9600);
   digitalWrite(13, LOW);
   delay(200);
   digitalWrite(13, HIGH);
-  delay(400);
+  delay(600);
 }
 
 void switchForever() {
-  digitalWrite(13, LOW);
-  digitalWrite(Mo1,HIGH);
+  digitalWrite(13, LOW);  //light turns off
+  digitalWrite(Mo1,HIGH); //motors spin one way
   digitalWrite(Mo3,HIGH);
     delay(2000);
     
   digitalWrite(13, HIGH); //light turns on
-  digitalWrite(Mo1,LOW);
+  digitalWrite(Mo1,LOW);  //motors spin opposite
   digitalWrite(Mo3,LOW);
     delay(1000);
 }
 
-
-void buttonSwitch() {
-  bState = digitalRead(butTon);
-  digitalWrite(Mo1, bState);
-  digitalWrite(Mo3, bState);
-  digitalWrite(13, HIGH);
-  delay(250);
-//  Serial.println("Print\n");
-  digitalWrite(13,LOW);
-  delay(250);
-}
-
 void loop() {
-
-switchForever();
-//buttonSwitch();
-  
-}         //end VOID LOOP
+switchForever(); 
+}
